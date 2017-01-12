c944723e491b6325da67f7dd860bfe4bee253e9d
Droopy06 <lamootalexandre@gmail.com>
2017-01-12 16:18:35 +0100

    add itemController

--- src/main/java/com/rizomm/ecommerce/Controller/ItemController.java
+++ src/main/java/com/rizomm/ecommerce/Controller/ItemController.java
@@ -1,14 +1,28 @@
 package com.rizomm.ecommerce.Controller;
 
 import com.rizomm.ecommerce.dao.ItemDaoImpl;
+import com.rizomm.ecommerce.factory.ItemFactory;
+import com.rizomm.ecommerce.model.Item;
 
 import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.util.List;
 
 /**
  * Created by ISEN on 07/01/2017.
  */
+@RequestScoped
+@Named
 public class ItemController {
     @EJB
     ItemDaoImpl items;
 
+    public String viewAllItems(){
+        return "produits.xhtml";
+    }
+
+    public List<Item> getItemList(){
+        return ItemFactory.getAllItems();
+    }
 }
